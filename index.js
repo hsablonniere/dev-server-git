3bd7e199fe91831496e96be49decec62ba685a29
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2022-08-12 17:26:08 +0200

    Fix import paths in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import devServerGitPlugin from 'src/dev-server-git.js';
-import viteGitPlugin from 'src/vite-plugin-git.js';
+import devServerGitPlugin from './src/dev-server-git.js';
+import viteGitPlugin from './src/vite-plugin-git.js';
 
 export { devServerGitPlugin, viteGitPlugin };
